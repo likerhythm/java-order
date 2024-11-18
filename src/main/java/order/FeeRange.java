db35d382b917544fa48eb9ade56aa1795926936b
likerhythm <wjdalsdl0412@gmail.com>
2024-11-18 15:58:32 +0900

    feat(fee range): 금액 범위 클래스

---
+++ src/main/java/order/FeeRange.java
@@ -0,0 +1,16 @@
+package order;
+
+public class FeeRange {
+
+    private final long min;
+    private final long max;
+
+    public FeeRange(final long min, final long max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public boolean isInRange(final long orderCost) {
+        return min <= orderCost && orderCost <= max;
+    }
+}
