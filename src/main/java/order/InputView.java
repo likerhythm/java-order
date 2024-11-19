994717f01afcd3a22c0ac5bbff0a7dd55eb11152
likerhythm <wjdalsdl0412@gmail.com>
2024-11-19 12:37:19 +0900

    feat(view): 입력 기능

---
+++ src/main/java/order/InputView.java
@@ -0,0 +1,31 @@
+package order;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String ORDER_INPUT_UNIT_FORMAT = "[가-힣a-zA-Z0-9]+\\(\\d개\\)";
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getOrder() {
+        String input = scanner.nextLine();
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("주문 형식이 잘못되었습니다.");
+        }
+        String[] splitInput = input.split(", ");
+        for (String split : splitInput) {
+            System.out.println(split);
+            if (!split.matches(ORDER_INPUT_UNIT_FORMAT)) {
+                throw new IllegalArgumentException("주문 형식이 잘못되었습니다.");
+            }
+        }
+//        if (!input.matches(ORDER_INPUT_UNIT_FORMAT)) {
+//            throw new IllegalArgumentException("주문 형식이 잘못되었습니다.");
+//        }
+        return input;
+    }
+}
---
+++ src/test/java/order/InputViewTest.java
@@ -0,0 +1,42 @@
+package order;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class InputViewTest {
+
+    InputView inputView;
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "콜라(2개)",
+            "콜라(2개), 햄버거(1개)"
+    })
+    void validOrderInputTest(String input) {
+        inputView = new InputView(new Scanner(new ByteArrayInputStream(input.getBytes())));
+        Assertions.assertDoesNotThrow(() -> inputView.getOrder());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            " ",
+            "콜라(2)",
+            "콜라(2개)/ 햄버거(1개)",
+            "콜라(2개",
+            "(2개)",
+            "(2개), 햄버거(1개)",
+            "콜라 (1개)",
+            "콜라(1 개)",
+            "콜라(한개)",
+            ", 햄버거(1개)",
+            "콜라(1개),\n햄버거(1개)"
+    })
+    void invalidOrderInputTest(String input) {
+        inputView = new InputView(new Scanner(new ByteArrayInputStream(input.getBytes())));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputView.getOrder());
+    }
+}
